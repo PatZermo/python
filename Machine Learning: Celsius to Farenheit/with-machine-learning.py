494990bf43ca131863aef8c2ed2618bbf33355d4
Patricio Zermo Dopico <65505080+PatZermo@users.noreply.github.com>
2024-08-19 17:44:46 -0300

    Create with-machine-learning.py

---
+++ Machine Learning: Celsius to Farenheit/with-machine-learning.py
@@ -0,0 +1,46 @@
+import tensorflow as tf
+import numpy as np
+
+celsius = np.array([-40, -10, 0, 8, 15, 22, 38], dtype=float)
+fahrenheit = np.array([-40, 14, 32, 46.4, 59, 71.6, 100.4], dtype=float)
+
+#capa = tf.keras.layers.Dense(units=1, input_shape=[1])
+#modelo = tf.keras.Sequential([capa])
+
+oculta1 = tf.keras.layers.Dense(units=3, input_shape=[1])
+oculta2 = tf.keras.layers.Dense(units=3)
+salida = tf.keras.layers.Dense(units=1)
+modelo = tf.keras.Sequential([oculta1, oculta2, salida])
+
+modelo.compile(
+    optimizer=tf.keras.optimizers.Adam(0.1),
+    loss='mean_squared_error'
+)
+
+print("Comenzando entrenamiento...")
+historial = modelo.fit(celsius, fahrenheit, epochs=1000, verbose=False)
+print("Modelo entrenado!")
+
+# En el modelo se incluyen 1000 vueltas pero puede verse en el gráfico que con menos de
+# 100 ya no tiene perdidas.
+import matplotlib.pyplot as plt
+plt.xlabel("# Epoca")
+plt.ylabel("Magnitud de pérdida")
+plt.plot(historial.history["loss"])
+plt.show()
+
+print("Variables internas del modelo")
+print(oculta1.get_weights())
+print(oculta2.get_weights())
+print(salida.get_weights())
+
+while True:
+    print("Hagamos una predicción!")
+    my_array = []
+    my_array.append(float(input("Ingresa una temperatura celsius (o escribe 7000 para salir): ")))
+    my_array = np.array(my_array)
+    resultado = modelo.predict(my_array)
+    if my_array == [7000]:
+        break
+    else:
+        print("El resultado es " + str(resultado) + " fahrenheit!")
